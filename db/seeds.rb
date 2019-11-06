2d17dd4c91147646b7d29037d4e33c78e403f554
Stephanie Voigt <stephanie.a.voigt@gmail.com>
2019-11-06 12:56:02 +0100

    Schema, seeds and Model created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = %w(chinese italian japanese french belgian)
+
+5.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category:  categories.sample(1).first
+  )
+  restaurant.save!
+end
+
